fa4e7f796d6115c4727451dba1dd70602762de4d
13567436138 <195446040@qq.com>
2018-04-13 17:06:51 +0800

    111

---
+++ src/main/java/com/youben/batch/entity/Message.java
@@ -0,0 +1,47 @@
+package com.youben.batch.entity;
+
+import java.util.Date;
+
+/*
+*hxp(hxpwangyi@126.com)
+*
+*/
+public class Message {
+	private Long messageId;
+	private Date receiveTime;
+	private String content;
+	private String receiver;
+	private int type;
+	public Long getMessageId() {
+		return messageId;
+	}
+	public void setMessageId(Long messageId) {
+		this.messageId = messageId;
+	}
+	public Date getReceiveTime() {
+		return receiveTime;
+	}
+	public void setReceiveTime(Date receiveTime) {
+		this.receiveTime = receiveTime;
+	}
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+	public String getReceiver() {
+		return receiver;
+	}
+	public void setReceiver(String receiver) {
+		this.receiver = receiver;
+	}
+	public int getType() {
+		return type;
+	}
+	public void setType(int type) {
+		this.type = type;
+	}
+	
+	
+}
