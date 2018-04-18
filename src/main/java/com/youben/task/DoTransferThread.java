26cc743822034ac54b244e88bc3b860b1bdb8dc3
13567436138 <195446040@qq.com>
2018-04-18 19:38:56 +0800

    111111

--- src/main/java/com/youben/task/DoTransferThread.java
+++ src/main/java/com/youben/task/DoTransferThread.java
@@ -5,7 +5,10 @@ import com.alibaba.druid.pool.DruidPooledConnection;
 import com.youben.constant.DataTransferConst;
 import com.youben.entity.TaskThread;
 
-import java.sql.SQLException;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 
 /**
  * Description:
@@ -29,11 +32,139 @@ public class DoTransferThread extends Thread {
     public void run() {
         DruidPooledConnection fromConn = null;
         DruidPooledConnection toConn=null;
+        ResultSet colRet=null;
+
         try {
             fromConn =from.getConnection();
             toConn=to.getConnection();
-            if(type== DataTransferConst.DO_JOB_TYPE_NEW){
+            //使用脚本建表
+            //JdbcUtil.createDestTables(fromConn,toConn);
 
+            if(type== DataTransferConst.DO_JOB_TYPE_NEW){
+                String tableName=taskThread.getTableName();
+
+                DatabaseMetaData fromMetaData=fromConn.getMetaData();
+                colRet = fromMetaData.getColumns(null, "%", tableName, "%");
+                int round=taskThread.getRecordCount()/DataTransferConst.COUNT_PER_QUERY;
+                if(taskThread.getRecordCount()%DataTransferConst.COUNT_PER_QUERY!=0){
+                    round++;
+                }
+                Date fromDate=taskThread.getRecordStartTime();
+                for(int i=0;i<round;i++) {
+                    String insertSql="replace  into "+tableName+"(";
+                    String valuesSql="values";
+                    PreparedStatement ps1=null;
+                    PreparedStatement ps2=null;
+                    PreparedStatement ps3=null;
+                    PreparedStatement ps4=null;
+                    ResultSet rs1=null;
+                    ResultSet rs2=null;
+                    ResultSet rs3=null;
+                    try {
+                        if (tableName.equals("sequences")) {
+                            ps1=fromConn.prepareStatement("select last_modified from " + tableName + " where last_modified>? limit " + DataTransferConst.COUNT_PER_QUERY + ",1");
+                            ps1.setDate(1,new java.sql.Date(fromDate.getTime()));
+                            rs1=ps1.executeQuery();
+                            Date toDate=null;
+                            if(rs1.next()){
+                                toDate = rs1.getDate(1);
+                            }
+                            if(toDate.getTime()>taskThread.getRecordEndTime().getTime()){
+                                toDate=taskThread.getRecordEndTime();
+                            }
+                            ps2=fromConn.prepareStatement("select * from " + tableName + " where last_modified>? and last_modified<=?  ");
+                            ps2.setDate(1,new java.sql.Date(fromDate.getTime()));
+                            ps2.setDate(2,new java.sql.Date(toDate.getTime()));
+                            rs2=ps2.executeQuery();
+                            fromDate=toDate;
+
+                            List<Integer> typeList=new ArrayList<>();
+                            while (colRet.next()) {
+                                String columnName = colRet.getString("COLUMN_NAME");
+                                int type = colRet.getInt("DATA_TYPE ");
+                                typeList.add(type);
+                                insertSql += columnName + ",";
+                            }
+
+                            ps3=fromConn.prepareStatement("select count(*) from " + tableName + " where last_modified>? and last_modified<=?  ");
+                            ps3.setDate(1,new java.sql.Date(fromDate.getTime()));
+                            ps3.setDate(2,new java.sql.Date(toDate.getTime()));
+                            rs3=ps3.executeQuery();
+                            if(rs3.next()){
+                                int count=rs3.getInt(1);
+                                String singleValue="(";
+                                for(int k=0;k<typeList.size();k++){
+                                    singleValue+="?,";
+                                }
+                                singleValue=singleValue.substring(0,singleValue.length()-1);
+                                singleValue+=")";
+                                for(int k=0;k<count;k++){
+                                    valuesSql+=singleValue+",";
+                                }
+                                valuesSql=valuesSql.substring(0,valuesSql.length()-1);
+                            }
+
+                            insertSql=insertSql.substring(0,insertSql.length()-1);
+                            insertSql+=valuesSql;
+
+                            ps4=toConn.prepareStatement(insertSql);
+
+                            int index=1;
+                            while(rs2.next()){
+                                for(int j=0;j<typeList.size();j++){
+                                    setParamter(ps4,typeList.get(j),index,rs2,j+1);
+                                    index++;
+                                }
+                            }
+
+                            ps4.executeUpdate();
+
+                        } else {
+                            ps1=fromConn.prepareStatement("select last_modified from " + tableName + " where gmt_modified>? limit " + DataTransferConst.COUNT_PER_QUERY + ",1");
+                            ps1.setDate(1,new java.sql.Date(fromDate.getTime()));
+                            rs1=ps1.executeQuery();
+                            Date toDate=null;
+                            if(rs1.next()){
+                                toDate = rs1.getDate(1);
+                            }
+                            if(toDate.getTime()>taskThread.getRecordEndTime().getTime()){
+                                toDate=taskThread.getRecordEndTime();
+                            }
+                            ps2=fromConn.prepareStatement("select * from " + tableName + " where gmt_modified>? and gmt_modified<=?  ");
+                            ps2.setDate(1,new java.sql.Date(fromDate.getTime()));
+                            ps2.setDate(2,new java.sql.Date(toDate.getTime()));
+                            rs2=ps2.executeQuery();
+                            fromDate=toDate;
+
+                        }
+
+
+                    }catch (Exception e){
+                        e.printStackTrace();
+                    }finally {
+                        if(ps1!=null){
+                            ps1.close();
+                        }
+                        if(ps2!=null){
+                            ps2.close();
+                        }
+                        if(rs1!=null){
+                            rs1.close();
+                        }
+                        if(rs2!=null){
+                            rs2.close();
+                        }
+                        if(ps3!=null){
+                            ps3.close();
+                        }
+                        if(rs3!=null){
+                            rs3.close();
+                        }
+                        if(ps4!=null){
+                            ps4.close();
+                        }
+                    }
+                }
             }else if(type==DataTransferConst.DO_JOB_TYPE_REDO){
 
             }
@@ -50,6 +181,96 @@ public class DoTransferThread extends Thread {
             } catch (SQLException e) {
                 e.printStackTrace();
             }
+            try {
+                colRet.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+
+    private void setParamter(PreparedStatement ps4,int type,int index,ResultSet rs2,int j) throws SQLException {
+        switch (type){
+            case Types.BOOLEAN:
+                ps4.setBoolean(index,rs2.getBoolean(j));
+                break;
+            case Types.BIT:
+                ps4.setByte(index,rs2.getByte(j));
+                break;
+            case Types.BIGINT:
+                ps4.setLong(index,rs2.getLong(j));
+                break;
+            case Types.INTEGER:
+                ps4.setInt(index,rs2.getInt(j));
+                break;
+            case Types.CHAR:
+                ps4.setString(index,rs2.getString(j));
+                break;
+            case Types.TINYINT:
+                ps4.setByte(index,rs2.getByte(j));
+                break;
+            case Types.DATE:
+                ps4.setDate(index,rs2.getDate(j));
+                break;
+            case Types.DECIMAL:
+                ps4.setBigDecimal(index,rs2.getBigDecimal(j));
+                break;
+            case Types.DOUBLE:
+                ps4.setDouble(index,rs2.getDouble(j));
+                break;
+            case Types.FLOAT:
+                ps4.setDouble(index,rs2.getFloat(j));
+                break;
+            case Types.JAVA_OBJECT:
+                ps4.setObject(index,rs2.getObject(j));
+                break;
+            case Types.LONGNVARCHAR:
+                ps4.setString(index,rs2.getString(j));
+                break;
+            case Types.LONGVARBINARY:
+                ps4.setBytes(index,rs2.getBytes(j));
+                break;
+            case Types.LONGVARCHAR:
+                ps4.setString(index,rs2.getString(j));
+                break;
+            case Types.NCHAR:
+                ps4.setString(index,rs2.getString(j));
+                break;
+            case Types.NCLOB:
+                ps4.setBytes(index,rs2.getBytes(j));
+                break;
+            case Types.NULL:
+                ps4.setObject(index,rs2.getObject(j));
+                break;
+            case Types.NUMERIC:
+                ps4.setDouble(index,rs2.getDouble(j));
+                break;
+            case Types.NVARCHAR:
+                ps4.setString(index,rs2.getString(j));
+                break;
+            case Types.OTHER:
+                ps4.setObject(index,rs2.getObject(j));
+                break;
+            case Types.REAL:
+                ps4.setDouble(index,rs2.getDouble(j));
+                break;
+            case Types.SMALLINT:
+                ps4.setShort(index,rs2.getShort(j));
+                break;
+            case Types.TIME:
+                ps4.setTime(index,rs2.getTime(j));
+                break;
+            case Types.TIMESTAMP:
+                ps4.setTimestamp(index,rs2.getTimestamp(j));
+                break;
+            case Types.VARBINARY:
+                ps4.setBytes(index,rs2.getBytes(j));
+                break;
+            case Types.VARCHAR:
+                ps4.setString(index,rs2.getString(j));
+                break;
+
         }
     }
 }
--- src/main/java/com/youben/utils/JdbcUtil.java
+++ src/main/java/com/youben/utils/JdbcUtil.java
@@ -1,10 +1,11 @@
 package com.youben.utils;
 
+import com.alibaba.druid.pool.DruidPooledConnection;
 import com.youben.entity.Datasource;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Description:
@@ -56,5 +57,132 @@ public class JdbcUtil {
             e.printStackTrace();
         }
         return null;
+    }
+
+    public static void createDestTables(DruidPooledConnection fromConn,DruidPooledConnection toConn){
+        List<String> tableNameList=new ArrayList<String>();
+        ResultSet rs0=null;
+
+        try {
+            DatabaseMetaData fromMetaData=fromConn.getMetaData();
+            rs0 =fromMetaData .getTables(null, null, null, new String[]{"TABLE"});
+            while (rs0.next()) {
+                tableNameList.add(rs0.getString("TABLE_NAME"));
+            }
+            for(int i=0;i<tableNameList.size();i++) {
+                String createTableSql="create table "+tableNameList.get(i)+"(";
+                ResultSet colRet=null;
+                ResultSet rs1=null;
+                try {
+                    rs1 = toConn.getMetaData().getTables(null, null, tableNameList.get(i), null);
+                    if (!rs1.next()) {
+                        colRet = fromMetaData.getColumns(null, "%", tableNameList.get(i), "%");
+                        while (colRet.next()) {
+                            String columnName = colRet.getString("COLUMN_NAME");
+                            String columnType = colRet.getString("TYPE_NAME");
+                            int type = colRet.getInt("DATA_TYPE ");
+                            int datasize = colRet.getInt("COLUMN_SIZE");
+                            int digits = colRet.getInt("DECIMAL_DIGITS");
+                            int nullable = colRet.getInt("NULLABLE");
+                            switch (type){
+                                case Types.BIGINT:
+                                    String isAutoIncrement=colRet.getString("IS_AUTOINCREMENT ");
+                                    createTableSql+=columnName+" bigint("+datasize+")";
+                                    if(isAutoIncrement.equals("YES")){
+                                        createTableSql+=" auto_invrement";
+                                    }
+                                    if(nullable==1){
+                                        createTableSql+=" null,";
+                                    }else{
+                                        createTableSql+=" not null,";
+                                    }
+                                    break;
+                                case Types.VARCHAR:
+                                    createTableSql+=columnName+" varchar("+datasize+")";
+                                    if(nullable==1){
+                                        createTableSql+=" null,";
+                                    }else{
+                                        createTableSql+=" not null,";
+                                    }
+                                    break;
+                                case Types.BIT:
+                                    createTableSql+=columnName+" varchar("+datasize+")";
+                                    if(nullable==1){
+                                        createTableSql+=" null,";
+                                    }else{
+                                        createTableSql+=" not null,";
+                                    }
+                                    break;
+                                case Types.TINYINT:
+                                    createTableSql+=columnName+" tinyint("+datasize+")";
+                                    if(nullable==1){
+                                        createTableSql+=" null,";
+                                    }else{
+                                        createTableSql+=" not null,";
+                                    }
+                                    break;
+                                case Types.DATALINK:
+                                    createTableSql+=columnName+" datetime";
+                                    if(nullable==1){
+                                        createTableSql+=" null,";
+                                    }else{
+                                        createTableSql+=" not null,";
+                                    }
+                                    break;
+                                case Types.INTEGER:
+                                    createTableSql+=columnName+" int("+datasize+")";
+                                    if(nullable==1){
+                                        createTableSql+=" null,";
+                                    }else{
+                                        createTableSql+=" not null,";
+                                    }
+                                    break;
+                                case Types.DOUBLE:
+                                    createTableSql+=columnName+" double";
+                                    if(nullable==1){
+                                        createTableSql+=" null,";
+                                    }else{
+                                        createTableSql+=" not null,";
+                                    }
+                                    break;
+                                case Types.LONGVARCHAR:
+                                    createTableSql+=columnName+" mediumtext";
+                                    if(nullable==1){
+                                        createTableSql+=" null,";
+                                    }else{
+                                        createTableSql+=" not null,";
+                                    }
+                                    break;
+                            }
+
+                        }
+                    }
+                }catch (Exception e){
+                    e.printStackTrace();
+                }finally {
+                    try {
+                        rs1.close();
+                    } catch (SQLException e) {
+                        e.printStackTrace();
+                    }
+
+                    try {
+                        colRet.close();
+                    } catch (SQLException e) {
+                        e.printStackTrace();
+                    }
+
+                }
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }finally {
+            try {
+                rs0.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+
+        }
     }
 }
